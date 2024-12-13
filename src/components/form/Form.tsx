58c9e2f3b2c2806fafb338f0a3213ca2900d4b99
deibidu <davidlopezfaustino@gmail.com>
2024-12-13 09:30:43 +0100

    retoques de css

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -8,7 +8,7 @@ export const Form = () => {
         <form action="/submit" className="form_container" method="POST">
           <div className="form_container_label d-flex-col">
             <label className="title-form" htmlFor="email">
-              Email
+              Email <span className="text-error">*</span>
             </label>
             <input
               type="email"
@@ -17,11 +17,10 @@ export const Form = () => {
               placeholder="example@email.com"
               required
             />
-            <p className="text-error">Escriu un missatge d'error</p>
           </div>
           <div className="form_container_label d-flex-col">
             <label className="title-form" htmlFor="name">
-              Nom i cognoms
+              Nom i cognoms <span className="text-error">*</span>
             </label>
             <input
               type="text"
@@ -30,11 +29,10 @@ export const Form = () => {
               placeholder="Escriu ací..."
               required
             />
-            <p className="text-error">Escriu un missatge d'error</p>
           </div>
           <div className="form_container_label d-flex-col">
             <label className="title-form" htmlFor="message">
-              Cuestió
+              Cuestió <span className="text-error">*</span>
             </label>
             <textarea
               className="form_container_label_input text"
@@ -42,7 +40,6 @@ export const Form = () => {
               placeholder="Escriu ací..."
               required
             ></textarea>
-            <p className="text-error">Escriu un missatge d'error</p>
           </div>
         </form>
         <button className="button-secondary" type="submit">
